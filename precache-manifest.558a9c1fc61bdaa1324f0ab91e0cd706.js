36f55d7978b5a44b74f0c0bafb08f831a76a0a24
islam saeed <43691810+eslamSaeed12@users.noreply.github.com>
2019-07-07 19:18:45 +0200

    Updates

--- precache-manifest.558a9c1fc61bdaa1324f0ab91e0cd706.js
+++ precache-manifest.558a9c1fc61bdaa1324f0ab91e0cd706.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "32d7c2516417b4a0881402927daeb847",
+    "revision": "e6362319703e4d7eadd0c30501dea55e",
     "url": "/quotime/index.html"
   },
   {
-    "revision": "046b670ff5ef9136c07c",
-    "url": "/quotime/static/css/main.29c15283.chunk.css"
+    "revision": "367681628dbba709ea73",
+    "url": "/quotime/static/css/main.e44ab696.chunk.css"
   },
   {
     "revision": "d54cb529fc2b1706f8a5",
     "url": "/quotime/static/js/2.1ae4b37a.chunk.js"
   },
   {
-    "revision": "046b670ff5ef9136c07c",
-    "url": "/quotime/static/js/main.4d4f8dba.chunk.js"
+    "revision": "367681628dbba709ea73",
+    "url": "/quotime/static/js/main.216c0390.chunk.js"
   },
   {
     "revision": "6d499ed5e5c4bc1ee553",
